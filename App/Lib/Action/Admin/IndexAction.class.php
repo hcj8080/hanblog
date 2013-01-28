d50a85eb0431c7785210ded7f0e301fb1c195b2d
MrHan <MrHan@lenovo>
2013-01-28 21:25:19 +0800

    add

--- App/Lib/Action/Admin/IndexAction.class.php
+++ App/Lib/Action/Admin/IndexAction.class.php
@@ -4,7 +4,10 @@ class IndexAction extends Action {
 
     public function index() {
         $this->display();
-    }   
+    }
+    public function add(){
+        $this->display();
+    }
 }
 
 ?>
